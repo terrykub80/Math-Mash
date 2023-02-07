7e23d71599bb2e77e1d0eb01639e3b1ec5c4f67e
Terry Kubalewski <terrykub@yahoo.com>
2023-02-07 13:58:49 -0600

    added Flask-Login and began seting up User class for login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from config import Config
 
@@ -11,6 +12,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
 
 # import all of the routes from the routes file into the current folder
 from . import routes, models
