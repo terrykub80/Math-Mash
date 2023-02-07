6f993aec84a47346521bca39ee590c349de6447f
Terry Kubalewski <terrykub@yahoo.com>
2023-02-07 11:54:31 -0600

    added handling to SignUpForm submission

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import random
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import SignUpForm
 
 
@@ -40,7 +40,20 @@ def posts():
     return 'These are your scores!'
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
+    if form.validate_on_submit():
+        print('Form Submitted and Validated!')
+        # Get data from the form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # TODO: Check to see if there is a User with username and/or email
+        # TODO: Create a new User with form data and add to database
+
+        # Redirect back to Home
+        return redirect(url_for('index'))
+        
     return render_template('signup.html', form=form)
